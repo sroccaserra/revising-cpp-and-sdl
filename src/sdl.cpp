ed6e8f3433625903dfadf6bc30aa19074010e585
sroccaserra <sroccaserra@yahoo.com>
2024-02-10 21:00:05 +0100

    r Extract run function

--- src/sdl.cpp
+++ src/sdl.cpp
@@ -3,29 +3,39 @@
 
 #include "State.h"
 
+void run();
+
 int main() {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         std::cerr << "Could not initialize SDL: " << SDL_GetError() << std::endl;
         return 1;
     }
 
-    {
-        State state(480, 270);
-        if (state.shouldQuit) {
-            return 1;
-        }
-
-        state.draw();
-
-        SDL_Event event;
-        while (!state.shouldQuit) {
-            while(SDL_PollEvent(&event)) {
-                state.update(event);
-            }
-            SDL_Delay(100);
-        }
+    int result {0};
+    try {
+        run();
+    }
+    catch(std::exception& e) {
+        result = 1;
     }
 
     SDL_Quit();
-    return 0;
+    return result;
+}
+
+void run() {
+    State state(480, 270);
+    if (state.shouldQuit) {
+        throw std::exception();
+    }
+
+    state.draw();
+
+    SDL_Event event;
+    while (!state.shouldQuit) {
+        while(SDL_PollEvent(&event)) {
+            state.update(event);
+        }
+        SDL_Delay(100);
+    }
 }
