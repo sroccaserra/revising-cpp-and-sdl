9b989e7052f309d5c692366d5547b8c545d2f408
sroccaserra <sroccaserra@yahoo.com>
2024-02-18 18:05:13 +0100

    r Use a list of strings for tile maps

--- config.lua
+++ config.lua
@@ -1,61 +1,78 @@
-local r = 1 -- top right corner
-local m = 2 -- top middle
-local l = 3 -- top left corner
-local b = 4 -- bottom
+---
+-- Utilities
 
-_G.menu_bar_tiles = {
-  {
-    r,m,m,m,m,m,m,m,m,m,m,m,m,m,m,m,m,m,m,m,m,m,m,m,m,m,m,m,m,m,
-    m,m,m,m,m,m,m,m,m,m,m,m,m,m,m,m,m,m,m,m,m,m,m,m,m,m,m,m,m,l,
-  },
-  {
-    b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,
-    b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,
-  },
-}
+local function convert_strings_to_tile_indexes(strings, map)
+  local result = {}
+  for _,line in ipairs(strings) do
+    local indexes = {}
+    table.insert(result, indexes)
+    for i = 1,#line do
+      local c = line:sub(i,i)
+      table.insert(indexes, map[c])
+    end
+  end
+  return result
+end
 
-local C = 32 -- plain bricks
-local s = 64 -- plain sky
-local R = 65 -- sky with star
-local g = 96 -- ground
-local e = 97 -- earth
-local t = 98 -- water top
-local w = 99 -- water
-local b = 100 -- bubble
-
-_G.room_01 = {
-  {s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s},
-  {s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s},
-  {s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,R,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s},
-  {s,s,s,s,s,s,s,s,s,R,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,R,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s},
-  {s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s},
-  {s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,C,s,C,s,C,s,C,s,C,s,C,s,C,s,C,s,C,s,C,s,C,s,C,s,C,s,C,s,s,s,s,s,s,s,s,s,s,s,s,s,s},
-  {s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,s,s,s,s,s,s,s,s,s,s,s,s,s,s},
-  {s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,s,s,s,s,s,s,s,s,s,s,s,s,s,s},
-  {s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,s,s,s,s,s,s,s,s,s,s,s,s,s,s},
-  {s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,s,s,s,s,s,s,s,s,s,s,s,s,s,s},
-  {s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,s,s,s,s,s,s,s,s,s,s,s,s,s,s},
-  {s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,s,s,s,s,s,s,s,s,s,s,s,s,s,s},
-  {s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,s,s,s,s,s,s,s,s,s,s,s,s,s,s},
-  {s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,s,s,s,s,s,s,s,s,s,s,s,s,s,s},
-  {s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,s,s,s,s,s,s,s,s,s,s,s,s,s,s},
-  {s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,s,s,s,s,R,s,s,s,s,s,s,s,s,s},
-  {s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,s,s,s,s,s,s,s,s,s,s,s,s,s,s},
-  {s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,s,s,s,s,s,s,s,s,s,s,s,s,s,s},
-  {s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,s,s,s,s,s,s,s,s,s,s,s,s,s,s},
-  {s,s,s,R,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,s,s,s,s,s,s,s,s,s,s,s,s,s,s},
-  {s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,s,s,s,s,s,s,s,s,s,s,s,s,s,s},
-  {s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,s,s,s,s,s,s,s,s,s,s,s,s,s,s},
-  {s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,s,s,s,s,s,s,s,s,s,s,R,s,s,s},
-  {s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,s,s,s,s,s,s,s,s,s,s,s,s,s,s},
-  {s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,s,s,s,s,s,s,s,s,s,s,s,s,s,s},
-  {s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,s,s,s,s,s,s,s,s,s,s,s,s,s,s},
-  {s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,s,s,s,s,s,s,s,s,s,s,s,s,s,s},
-  {s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,s,s,s,s,s,s,s,s,s,s,s,s,s,s},
-  {s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,s,s,s,s,s,s,s,s,s,s,s,s,s,s},
-  {g,g,g,g,g,g,g,g,g,g,t,t,t,t,t,t,t,t,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g},
-  {e,e,e,e,e,e,e,e,e,e,w,w,w,w,w,b,w,w,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e},
-  {e,e,e,e,e,e,e,e,e,e,w,w,w,w,w,w,w,w,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e},
-}
+---
+-- Definitions
 
 _G.cursor_tiles = {1, 0, 2, 2}
+
+_G.menu_bar_tiles = convert_strings_to_tile_indexes({
+  'rmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmml',
+  'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb',
+},{
+  r = 1,
+  m = 2,
+  l = 3,
+  b = 4,
+})
+
+local r_01 = {
+  'ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss',
+  'ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss',
+  'sssssssssssssssssssssRssssssssssssssssssssssssssssssssssssss',
+  'sssssssssRssssssssssssssssssssssssssssssssssRsssssssssssssss',
+  'ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss',
+  'sssssssssssssssssssCsCsCsCsCsCsCsCsCsCsCsCsCsCssssssssssssss',
+  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
+  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
+  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
+  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
+  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
+  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
+  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
+  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
+  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
+  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssRsssssssss',
+  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
+  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
+  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
+  'sssRsssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
+  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
+  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
+  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssRsss',
+  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
+  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
+  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
+  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
+  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
+  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
+  'ggggggggggttttttttgggggggggggggggggggggggggggggggggggggggggg',
+  'eeeeeeeeeewwwwwbwweeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
+  'eeeeeeeeeewwwwwwwweeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
+}
+
+local r_01_map = {
+  C = 32, -- plain bricks
+  s = 64, -- plain sky
+  R = 65, -- sky with star
+  g = 96, -- ground
+  e = 97, -- earth
+  t = 98, -- water top
+  w = 99, -- water
+  b = 100, -- bubble
+}
+
+_G.room_01 = convert_strings_to_tile_indexes(r_01, r_01_map)
