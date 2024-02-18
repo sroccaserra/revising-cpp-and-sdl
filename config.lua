7271f4ae5cd0c22cdac47fc3e664382c29424207
sroccaserra <sroccaserra@yahoo.com>
2024-02-18 22:54:41 +0100

    f Nicer castle

--- config.lua
+++ config.lua
@@ -30,46 +30,56 @@ _G.menu_bar_tiles = convert_strings_to_tile_indexes({
 })
 
 local r_01 = {
-  'ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss',
-  'ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss',
-  'sssssssssssssssssssssRssssssssssssssssssssssssssssssssssssss',
-  'sssssssssRssssssssssssssssssssssssssssssssssRsssssssssssssss',
-  'ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss',
-  'sssssssssssssssssssCsCsCsCsCsCsCsCsCsCsCsCsCsCssssssssssssss',
-  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
-  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
-  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
-  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
-  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
-  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
-  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
-  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
-  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
-  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssRsssssssss',
-  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
-  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
-  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
-  'sssRsssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
-  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
-  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
-  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssRsss',
-  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
-  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
-  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
-  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
-  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
-  'sssssssssssssssssssCCCCCCCCCCCCCCCCCCCCCCCCCCCssssssssssssss',
-  'ggggggggggttttttttgggggggggggggggggggggggggggggggggggggggggg',
-  'eeeeeeeeeewwwwwbwweeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
+  '.......................^.................^..................',
+  '......................qrs.......^.......qrs.................',
+  '.....................qrrrs.....qrs.....qrrrs................',
+  '.........R..........qrrrrrs...qrrrs...qrrrrrs...............',
+  '...................qrrrrrrrs.qrrrrrs.qrrrrrrrs..............',
+  '...................B.B.B.B.B.B.B.B.B.B.B.B.B.B..............',
+  '...................BBBBBBBBB.BBBBBBB.BBBBBBBBB..............',
+  '...................ABBBBBBBC.ABBBBBC.ABBBBBBBC..............',
+  '....................BBBBBBB...BCABB...BBBBBBB...............',
+  '....................BBBBBBB...B..BB...BBBBBBB...............',
+  '....................BBBBBBB...B..BB...BBBBBBB...............',
+  '....................BBBBBBBBBBBBBBBBBBBBBBBBC...............',
+  '....................BBBBBBBBBBBBBBBBBBBBBBBB................',
+  '....................ABBBBBBBBBBBBBBBBBBBBBBB................',
+  '.....................BBBBBBBBBBBBBBBBBBBBBBB................',
+  '.....................BBBBBBBBBBBBBBBBBBBBBBB......R.........',
+  '.....................BBBBBBBBBBBBBBBBBBBBBBC................',
+  '.....................BBBCABBBCABBBCABBCABBB.................',
+  '.....................BBB..BBB..BBB..BB..BBB.................',
+  '...R.................BBBBBBBBBBBBBBBBBBBBBB.................',
+  '.....................BBBBBBBBBBBBBBBBBBBBBB.................',
+  '.....................BBBBBBBBBBBBBBBBBBBBBB.................',
+  '.....................BBBBBBBBBBCABBBBBBBBBC.............R...',
+  '.....................BBBBBBBBBB..BBBBBBBBB..................',
+  '.....................BBBCABBBBB..BBBBBBBBB..................',
+  '.....................BBB..BBBBB..BBBBBBBBB...........gg.....',
+  '.....................BBB..BBBBB..BBBBCABBB.........ggee.....',
+  '.....................BBB..BBBBBBBBBBB..BBB.......ggeeee.....',
+  '.....................BBB..BBBBBBBBBBB..BBB.....ggeeeeee.....',
+  'ggggggggggttttttttgggggggggggggggggggggggggggggeeeeeeegggggg',
+  'eeeeeeeeeewwwwwwwweeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
   'eeeeeeeeeewwwwwwwweeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
 }
 
 local r_01_map = {
-  C = 32, -- plain bricks
-  s = 64, -- plain sky
+  A = 32, -- \ bricks
+  B = 33, -- plain bricks
+  C = 34, -- / brick
+
+  q = 66, -- roof left
+  r = 67, -- roof
+  s = 68, -- roof right
+  ['^'] = 69,
+
+  ['.'] = 64, -- plain sky
   R = 65, -- sky with star
+
   g = 96, -- ground
   e = 97, -- earth
+
   t = 98, -- water top
   w = 99, -- water
   b = 100, -- bubble
